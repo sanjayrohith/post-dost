2f02ca87da2b8c63dc64632a5b563107893ac0f2
sanjayrohith <sanjayrohith1802@gmail.com>
2025-09-20 14:56:25 +0530

    removed boilers

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/date-aware-post-suggestions.ts';
-import '@/ai/flows/generate-social-media-post.ts';
-import '@/ai/flows/generate-audio-from-caption.ts';
